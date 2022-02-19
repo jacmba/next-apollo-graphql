c7916b3c485231e6317e8c1360f12ea9f0c979ae
Jacinto Mba Cantero <jacmba@gmail.com>
2022-02-19 03:21:52 +0000

    Client side rendering

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import { ApolloProvider } from "@apollo/client";
 import "../styles/globals.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import client from "../services/apollo-client";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
 export default MyApp;
